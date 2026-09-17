9074558b1041623693553b4e5cb455e9b969e4b7
Kenneth Lynne Release <hello@kenneth.ly>
2026-09-17 10:01:28 +0000

    docs: update Cogenity 0.31.12 from ce8621d7b10d

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eu
 
 repository=kennethlynne/cogenity
-release_version=0.31.11
+release_version=0.31.12
 install_dir=${COGENITY_INSTALL_DIR:-"$HOME/.local/bin"}
 operating_system=$(uname -s)
 architecture=$(uname -m)
